8346f8b4deb901559ca70ebf4f554d58c51847dc
TuLe <tuleaibk@gmial.com>
2025-10-25 10:05:04 +0700

    fix models import

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 from ..extensions import db
 
-from .user_models import User, Role, UserInfor
+from .user_models import User, Role, UserInfor, Gender
 from .product_models import Product, Category, Brand, Ingredient, ProductIngredient
 from .ecommerce_models import (
     Cart, CartItem,
@@ -11,7 +11,7 @@ from .ecommerce_models import (
 
 __all__ = [
     'db',
-    'User', 'Role', 'UserInfor',
+    'User', 'Role', 'UserInfor', 'Gender',
     'Product', 'Category', 'Brand', 'Ingredient', 'ProductIngredient',
     'Cart', 'CartItem',
     'Order', 'OrderItem', 'OrderStatus',
